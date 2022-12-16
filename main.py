b59af9da937df640dd7e4a522d253c807716b8ce
juxqq <75832820+juxqq@users.noreply.github.com>
2022-12-16 01:03:17 +0100

    code nul 0/20

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
+# Imports
 import tweepy
 import openai
 import json
+import time
 
 # Import configuration variables
 with open('config.json') as config_file:
     data = json.load(config_file)
 
-bearer_token = data['bearer_token']
-key_api = data['key_api']
-bot_id = data['bot_id']
+# Credentials
+API_KEY = data['API_KEY']
+API_SECRET_KEY = data['API_SECRET_KEY']
+BEARER_TOKEN = data['BEARER_TOKEN']
+ACCESS_TOKEN = data['ACCESS_TOKEN']
+ACCESS_SECRET_TOKEN = data['ACCESS_SECRET_TOKEN']
+OPENAI_API_KEY = data['OPENAI_API_KEY']
+BOT_ID = data['BOT_ID']
 
-client = tweepy.Client(
-    bearer_token=bearer_token
-)
-openai.api_key = key_api
+# accès à l'API de Twitter
+client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_SECRET_TOKEN)
 
-class Tweetmentions(tweepy.StreamingClient):
+# accès à l'API d'OpenAI
+openai.api_key = OPENAI_API_KEY
+
+class tweetMentions(tweepy.StreamingClient):
+    def on_connect(self):
+        print("Connected.")
+
     def on_tweet(self, tweet):
         print(tweet.text)
         print(tweet.id)
         question = tweet.text
         response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=question,
-        max_tokens=280,
-        n=1,
-        stop=["?"]
-    )
+            engine="text-davinci-002",
+            prompt=question,
+            max_tokens=70,
+            n=1,
+            stop=["?"]
+        )
         print(response)
-
-stream_mentions = Tweetmentions(bearer_token=bearer_token)
-stream_mentions.add_rules(tweepy.StreamRule(bot_id)) 
+        try:
+            client.create_tweet(text=response, in_reply_to_tweet_id=tweet.id)
+        except:
+            print("Erreur")
+        time.sleep(15)
+    
+stream_mentions = tweetMentions(BEARER_TOKEN)
+stream_mentions.add_rules(tweepy.StreamRule(BOT_ID)) 
 stream_mentions.filter()
---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class test(unittest.TestCase):
+    def onConnect(self):
+        print("onConnect")
+
+if __name__ == '__main__':
+    unittest.main()
